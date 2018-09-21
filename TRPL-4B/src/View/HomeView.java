7657c5c67b7faa6b0d7a6e3400f11cd1bb61d41f
zahrofatimatuz <fazahoy@gmail.com>
2018-09-21 12:23:53 +0700

    View Home

--- TRPL-4B/src/View/HomeView.java
+++ TRPL-4B/src/View/HomeView.java
@@ -9,12 +9,12 @@ package View;
  *
  * @author adheraprabu
  */
-public class v_home extends javax.swing.JFrame {
+public class HomeView extends javax.swing.JFrame {
 
     /**
      * Creates new form v_home
      */
-    public v_home() {
+    public HomeView() {
         initComponents();
     }
 
@@ -33,46 +33,49 @@ public class v_home extends javax.swing.JFrame {
         Button_keluar = new javax.swing.JButton();
         Label_level1 = new javax.swing.JLabel();
         Label_level2 = new javax.swing.JLabel();
-        Judul = new javax.swing.JLabel();
         Background = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        Button_play.setText("Play");
+        Button_play.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/Home/btn-main.png"))); // NOI18N
+        Button_play.setBorderPainted(false);
+        Button_play.setContentAreaFilled(false);
         Button_play.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 Button_playActionPerformed(evt);
             }
         });
-        getContentPane().add(Button_play, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 560, -1, -1));
+        getContentPane().add(Button_play, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 660, 120, 50));
 
-        Button_Bantuan.setText("Bantuan");
-        getContentPane().add(Button_Bantuan, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 550, -1, -1));
+        Button_Bantuan.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/Home/btn-help.png"))); // NOI18N
+        Button_Bantuan.setBorderPainted(false);
+        Button_Bantuan.setContentAreaFilled(false);
+        getContentPane().add(Button_Bantuan, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 660, 140, 50));
 
-        Button_Tentang.setText("Tentang");
-        getContentPane().add(Button_Tentang, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 550, -1, -1));
+        Button_Tentang.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/Home/btn-about.png"))); // NOI18N
+        Button_Tentang.setBorderPainted(false);
+        Button_Tentang.setContentAreaFilled(false);
+        getContentPane().add(Button_Tentang, new org.netbeans.lib.awtextra.AbsoluteConstraints(920, 660, 140, 50));
 
-        Button_keluar.setText("Keluar");
-        getContentPane().add(Button_keluar, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, -1, -1));
+        Button_keluar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/Home/btn-exit.png"))); // NOI18N
+        Button_keluar.setBorderPainted(false);
+        Button_keluar.setContentAreaFilled(false);
+        getContentPane().add(Button_keluar, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 130, 60));
 
         Label_level1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         Label_level1.setText("Level 1");
         Label_level1.setOpaque(true);
-        getContentPane().add(Label_level1, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 220, 110, 140));
+        getContentPane().add(Label_level1, new org.netbeans.lib.awtextra.AbsoluteConstraints(890, 160, 110, 140));
 
         Label_level2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         Label_level2.setText("Level 2");
         Label_level2.setOpaque(true);
-        getContentPane().add(Label_level2, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 220, 110, 140));
-
-        Judul.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
-        Judul.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        Judul.setText("Wellcome");
-        getContentPane().add(Judul, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 80, -1, -1));
+        getContentPane().add(Label_level2, new org.netbeans.lib.awtextra.AbsoluteConstraints(1040, 160, 110, 140));
 
         Background.setBackground(new java.awt.Color(255, 102, 102));
         Background.setForeground(new java.awt.Color(255, 204, 204));
+        Background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/Home/bg-Home.png"))); // NOI18N
         Background.setOpaque(true);
         Background.setPreferredSize(new java.awt.Dimension(1200, 750));
         getContentPane().add(Background, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -91,7 +94,6 @@ public class v_home extends javax.swing.JFrame {
     private javax.swing.JButton Button_Tentang;
     private javax.swing.JButton Button_keluar;
     private javax.swing.JButton Button_play;
-    private javax.swing.JLabel Judul;
     private javax.swing.JLabel Label_level1;
     private javax.swing.JLabel Label_level2;
     // End of variables declaration//GEN-END:variables
